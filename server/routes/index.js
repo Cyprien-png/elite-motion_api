80c46bbb110aff7cf2a95c95cccc30143c2dc07e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-03-17 18:39:51 +0100

    refactor auth code
    Dispatch auth functions in a dedicated file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
-import db from "../db/index.js"
+import db from "../db/index.js";
+import auth from "./utils/auth.js";
 import jwt from "jsonwebtoken"
 import moment from "moment"
 
@@ -8,87 +9,16 @@ dotenv.config()
 const router = express.Router()
 
 
-//check if session is still valid
-const checkSession = (req) => {
-    return new Promise((resolve, reject) => {
-        let result = true
-
-        const authHeader = req.headers["authorization"];
-        const token = authHeader && authHeader.split(" ")[1];
-
-        result = jwt.verify(token, process.env.USER_SESSION_TOKEN_SECRET, { json: true }, async function (err, decoded) {
-
-            let sessions
-            let currentSession
-
-            if (!decoded || !decoded.user_id) {
-                resolve(false);
-            }
-
-            //getCurrentSession
-            try {
-                currentSession = (await db.getSessionByToken(token))
-            } catch (e) {
-                reject(e);
-            }
-
-            //get all tokens of current user 
-            try {
-                sessions = (await db.getUsersSessions(decoded.user_id))
-                if (!sessions || sessions.length < 1) {
-                    resolve(false);
-                }
-            } catch (e) {
-                console.log(e)
-                reject(e);
-            }
-            //check every tokens date and remove outdated
-            if (sessions && sessions.length > 0) {
-                for (const session of sessions) {
-                    if (moment(session.end_date, "YYYY-MM-DDTHH:mm:ssZ").format("x") < moment().format("x")) {
-                        await db.deleteUsersSession(session.users_session_id);
-                    }
-                }
-            }
-
-            //check if token is valid 
-            if (currentSession.length <= 0) {
-                resolve(false);
-
-            } else {
-                //if right, renew date 
-                try {
-                    await db.postponeSessionEnd(currentSession[0].users_session_id, moment(moment()).add(1, "M").format("YYYY-MM-DDTHH:mm:ssZ"))
-                } catch (e) {
-                    console.log(e)
-                    reject(e);
-                }
-            }
-        })
-        resolve(true);
-    })
-}
-
-
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split(" ")[1]
-    if (token == null) return res.sendStatus(401)
-
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
-}
-
-
-
-
 //Verify if mail and password match 
 router.post("/login", async (req, res) => {
 
     const user = req.body
+
+    if (!user || !user.mail || !user.password) {
+        res.sendStatus(500)
+        return
+    }
+
     let userData
 
     try {
@@ -103,30 +33,68 @@ router.post("/login", async (req, res) => {
     if (!user.password || !userData.password || user.password !== userData.password) {
         //wrong password
         res.sendStatus(401)
+
     } else {
-        let creationTime = moment().format("YYYY-MM-DDTHH:mm:ssZ")
-        let payload = { "mail": user.mail, "user_id": userData.user_id, "created_at": creationTime }
+        const token = auth.createToken(user.mail, userData.user_id)
 
-        const token = jwt.sign(payload, process.env.USER_SESSION_TOKEN_SECRET)
-
-        try {
-            //create session
-            await db.createUserSession(token, moment(creationTime).add(1, "M").format("YYYY-MM-DDTHH:mm:ssZ"), userData.user_id)
-
-            //send token
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'POST');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-            res
-                .json({ token: token })
-            //.sendStatus(200)
-
-        } catch (e) {
-            console.log(e)
-            res.sendStatus(500)
-        }
+        //Create session
+        auth.createSession(res, token, userData.user_id)
     }
+
+})
+
+
+
+//Create an account for the new user
+router.post("/signup", async (req, res) => {
+
+    const user = req.body
+
+    if (!user || !user.mail || !user.password) {
+        return res.sendStatus(500)
+
+    }
+
+    let ghostUser
+
+    try {
+        ghostUser = await db.getUser(user.mail)
+        if (ghostUser.length > 0) {
+            return res.sendStatus(409)
+
+        }
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+
+    //Create account
+    try {
+        await db.createUser(user.mail, user.password)
+        console.log("SUCCESS")
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+
+    //Read the new user fields in db to get its id
+    let userData
+    try {
+        userData = (await db.getUser(user.mail))[0]
+        //wrong email
+        if (!userData || !userData.mail || !userData.password) res.sendStatus(401)
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+
+
+    //create token
+    const token = auth.createToken(user.mail, userData.user_id)
+    console.log(token)
+
+    //Create session
+    auth.createSession(res, token, userData.user_id)
 
 })
 
@@ -134,8 +102,7 @@ router.post("/login", async (req, res) => {
 
 //Verify if user's token is still valid
 router.get("/sessionCheck", async (req, res) => {
-
-    checkSession(req)
+    auth.checkSession(req)
         .then((isValid) => {
             if (isValid) {
                 res.sendStatus(200)
---
+++ server/routes/utils/auth.js
@@ -0,0 +1,100 @@
+import dotenv from "dotenv";
+import db from "../../db/index.js"
+import jwt from "jsonwebtoken"
+import moment from "moment"
+
+dotenv.config()
+let auth = {}
+
+//create new token
+auth.createToken = (mail, user_id) => {
+    let creationTime = moment().format("YYYY-MM-DDTHH:mm:ssZ")
+    let payload = { "mail": mail, "user_id": user_id, "created_at": creationTime }
+
+    return jwt.sign(payload, process.env.USER_SESSION_TOKEN_SECRET)
+}
+
+
+auth.createSession = async (res, token, user_id) => {
+    try {
+        //create session
+        await db.createUserSession(token, moment(moment().format("YYYY-MM-DDTHH:mm:ssZ")).add(1, "M").format("YYYY-MM-DDTHH:mm:ssZ"), user_id)
+
+        //send token
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+        res
+            .json({ token: token })
+        //.sendStatus(200)
+
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+}
+
+
+//check if session is still valid
+auth.checkSession = (req) => {
+    return new Promise((resolve, reject) => {
+        let result = true
+
+        const authHeader = req.headers["authorization"];
+        const token = authHeader && authHeader.split(" ")[1];
+
+        result = jwt.verify(token, process.env.USER_SESSION_TOKEN_SECRET, { json: true }, async function (err, decoded) {
+
+            let sessions
+            let currentSession
+
+            if (!decoded || !decoded.user_id) {
+                resolve(false);
+            }
+
+            //getCurrentSession
+            try {
+                currentSession = (await db.getSessionByToken(token))
+            } catch (e) {
+                reject(e);
+            }
+
+            //get all tokens of current user 
+            try {
+                sessions = (await db.getUsersSessions(decoded.user_id))
+                if (!sessions || sessions.length < 1) {
+                    resolve(false);
+                }
+            } catch (e) {
+                console.log(e)
+                reject(e);
+            }
+            //check every tokens date and remove outdated
+            if (sessions && sessions.length > 0) {
+                for (const session of sessions) {
+                    if (moment(session.end_date, "YYYY-MM-DDTHH:mm:ssZ").format("x") < moment().format("x")) {
+                        await db.deleteUsersSession(session.users_session_id);
+                    }
+                }
+            }
+
+            //check if token is valid 
+            if (currentSession.length <= 0) {
+                resolve(false);
+
+            } else {
+                //if right, renew date 
+                try {
+                    await db.postponeSessionEnd(currentSession[0].users_session_id, moment(moment()).add(1, "M").format("YYYY-MM-DDTHH:mm:ssZ"))
+                } catch (e) {
+                    console.log(e)
+                    reject(e);
+                }
+            }
+            resolve(true);
+        })
+    })
+};
+
+export default auth
